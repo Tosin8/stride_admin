912496ecdb9062ca29e65c10501756b7d36d9849
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-07 14:09:41 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 Future<void> main()  async {
+
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize GetX local storage. 
+
+  // remove # sign from url 
+
+  // initialize firebase & authentication repository
+
+  // main app starts here..
   runApp(const MyApp());
 }
