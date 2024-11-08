a3c38190e7995ef1be483c199d2dd6503df6f6cd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-08 16:44:34 +0100

    Update main.dart
    Updating Main with Firebase Auth.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:stride_admin/firebase_options.dart';
 
+
+// Entry point of flutter web. 
 Future<void> main()  async {
 
 
@@ -10,6 +15,9 @@ Future<void> main()  async {
   // remove # sign from url 
 
   // initialize firebase & authentication repository
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then((_) => Get.put(AuthenticationRepository()));
 
   // main app starts here..
   runApp(const MyApp());
