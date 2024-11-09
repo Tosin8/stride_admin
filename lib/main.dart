c60301eb81f7fa78e577069722db36be016024ad
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-09 23:21:29 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stride_admin/firebase_options.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 
@@ -14,6 +15,7 @@ Future<void> main()  async {
   // initialize GetX local storage. 
 
   // remove # sign from url 
+  setPathUrlStrategy(); 
 
   // initialize firebase & authentication repository
 
