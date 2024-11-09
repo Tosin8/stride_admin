d360b938e32f9c744d3d9340f854bf2bfa56de90
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-09 23:32:29 +0100

    adding routes.

---
+++ lib/util/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String register = '/register';
+  static const String verifyMail = '/verifyMail';
+  static const String forgotPassword = '/forgotPassword';
+  static const String resetPassword = '/resetPassword'; 
+  
+}
