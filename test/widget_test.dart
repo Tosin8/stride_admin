040838849c1eaef20d205ad18b3723eb3cf029d2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-11 23:42:27 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shimmer/main.dart';
 
-import 'package:stride_admin/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
