09d94168f316acb8a01d2d13dc0b3c862654f2c2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-12 15:01:17 +0100

    Update routes.dart

--- lib/util/routes/routes.dart
+++ lib/util/routes/routes.dart
@@ -6,8 +6,34 @@ class Routes {
   static const String forgotPassword = '/forgotPassword';
   static const String resetPassword = '/resetPassword'; 
 
-  
+
   static const dashboard = '/dashboard'; 
   static const media = '/media'; 
-  
+
+
+  static const banners = '/banners' ; 
+  static const createBanner = '/createBanner'; 
+  static const editBanner = '/editBanner'; 
+
+
+  static const products = '/products'; 
+  static const createProduct = '/createProducts'; 
+  static const editProduct = '/editProduct'; 
+
+  static const categories = '/categories'; 
+  static const crateCategory = '/createCategory';
+  static const editCategory = '/editCategory'; 
+
+  static const brands = '/brands'; 
+  static const createBrand = '/createBrand'; 
+  static const editBrand = '/editBrand';
+
+  static const customers = '/customers'; 
+  static const createCustomer = '/createCustomr'; 
+  static const customerDetails = '/customerDetails'; 
+
+
+  static const orders = '/orders'; 
+
+
 }
