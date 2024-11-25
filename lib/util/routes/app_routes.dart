cb9542ba0c1ac1c870fb3e74bacf46651c7ce895
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-25 22:34:57 +0100

    Update app_routes.dart

--- lib/util/routes/app_routes.dart
+++ lib/util/routes/app_routes.dart
@@ -1,8 +1,14 @@
 import 'package:get/get.dart';
 
+import 'routes.dart';
+
 class AppRoutes {
   static final List<GetPage> pages = [
 GetPage(name: Routes.login, page: () => const LoginScreen()), 
+GetPage(name: Routes.forgotPassword, page: () => ForgetPwdScreen()), 
+GetPage(name: Routes.resetPassword, page: () => ResetPwdScreen()), 
+GetPage(name: Routes.dashboard, page: () => DashboardScreen(), middlewares: [RouteMiddleware()]),
+GetPage(name: Routes.media, page: () => MediaScreen(), middlewares: 
 
 
 
